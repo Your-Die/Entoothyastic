698df57fac70b87c0ae8187e669b665ff470d5e9
Jordy <jordy_van_opstal@live.nl>
2019-04-27 00:36:01 +0200

    Implement crappy mouth definition asset.

---
+++ Assets/Scripts/Mouth/MouthController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MouthController : MonoBehaviour
+{
+    [SerializeField] private List<Tooth> _teeth;
+
+    [SerializeField] private List<MouthDefinition> _mouthDefinitions;
+
+    [SerializeField] private bool _disableOnFinish = true;
+
+    private int _definitionIndex = 0;
+
+    private List<Tooth> _unmatchedTeeth;
+
+    public UnityEvent AllDefinitionsMatched;
+
+    private void OnEnable()
+    {
+        _definitionIndex = 0;
+
+        var definition = _mouthDefinitions.First();
+        SetDefinition(definition);
+    }
+
+    private void SetDefinition(MouthDefinition definition)
+    {
+        _unmatchedTeeth = definition.Apply(_teeth).ToList();
+
+        foreach (Tooth tooth in _unmatchedTeeth)
+            tooth.Matched.AddListener(OnToothMatched);
+    }
+
+    private void OnToothMatched(Tooth tooth)
+    {
+        tooth.Matched.RemoveListener(OnToothMatched);
+        _unmatchedTeeth.Remove(tooth);
+
+        if (!_unmatchedTeeth.Any())
+            OnDefinitionFinished();
+    }
+
+    private void OnDefinitionFinished()
+    {
+        _definitionIndex++;
+        if (_definitionIndex < _mouthDefinitions.Count)
+        {
+            MouthDefinition nextDefinition = _mouthDefinitions[_definitionIndex];
+            SetDefinition(nextDefinition);
+        }
+        else
+        {
+            AllDefinitionsMatched?.Invoke();
+
+            if (_disableOnFinish)
+                enabled = false;
+        }
+    }
+}
