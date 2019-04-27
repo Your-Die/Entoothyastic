816f75d6177a9d3435e6f6769fafc2abaa433096
Tudor <this.tudor@gmail.com>
2019-04-27 15:44:33 +0200

    HandAndBrushMover and ToothInfo
    Move hand & brush with lerp and animation curves for pos and rot, the rot is based on the nearest keyframe transforms defined. Then the brush action is a couple of lerps for up and down, with animation curves also. The tooth has info on letters needed and dirtiness, but they are not assigned to by anything.

---
+++ Assets/Scripts/Hand and brush Movement/HandAndBrushMover.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class HandAndBrushMover : MonoBehaviour
+{
+    [SerializeField]
+    Transform m_ToothbrushPositionerParent;
+    [SerializeField]
+    Transform m_ToothbrushBrushingChild;
+    [SerializeField]
+	float m_SpeedModifier = 1;
+	[SerializeField]
+	float animationDuration = 1;
+    [SerializeField]
+    float m_brushingMotionWidth = 0.1f;
+	//[SerializeField]
+	public List<Transform> RotationKeyframes_TopMouth;
+	//[SerializeField]
+	public List<Transform> RotationKeyframes_BottomMouth;
+    
+	[SerializeField]
+	AnimationCurve m_BrushTravelPosAnimCurve;
+    [SerializeField]
+	AnimationCurve m_BrushTravelRotAnimCurve;
+	[SerializeField]
+	AnimationCurve m_BrushingAnimCurve;
+	
+
+    Vector3 m_TargetPosLastFrame;
+
+	
+    void Start(){
+	}
+	
+	void Update(){
+	
+	}
+
+    /// <summary>
+    /// If the new target is different from the previous target, we move to it and then do 1x brush
+    /// If the new target is the same as the old target, we just do a 1x brush.
+    /// So you need to spam the same command multiple times to brush the tooth more than once.
+    /// </summary>
+    /// <param name="targetPos"></param>
+	public void MoveToTargetAndBrush(ToothInfo targetTooth){
+        Vector3 targetPos = targetTooth.transform.position;
+        //cache previous values if target is the same
+        bool isTargetTheSame = false;
+        Vector3 brushingUpDir = m_ToothbrushBrushingChild.right;//TODO: figure out depending on how hand is held
+        
+
+        if(Vector3.Distance(m_TargetPosLastFrame, targetPos) < 0.01f)
+            isTargetTheSame = true;
+
+        if(!isTargetTheSame){
+            // calculate keyframe blended rotation
+            #region Get Rotation From nearby Keyframes
+                Quaternion targetRot = targetTooth.GetClosestBlendedKeyframeRotation(2,false);
+            #endregion
+
+            // LerpToTarget pos and rot to target
+            StartCoroutine(LerpToTarget(
+                m_ToothbrushPositionerParent,
+                m_ToothbrushPositionerParent.position,
+                targetPos,
+                m_ToothbrushPositionerParent.rotation,
+                targetRot,
+                m_BrushTravelPosAnimCurve,
+                m_BrushTravelRotAnimCurve,
+                0, 0, 0,
+                animationDuration, m_SpeedModifier,
+                ()=>{BrushTooth1x_Toggled(m_ToothbrushBrushingChild,brushingUpDir);}
+            ));
+        }
+        else{
+            // brush teeth 1x
+            BrushTooth1x_Toggled(m_ToothbrushBrushingChild, brushingUpDir);
+        }
+
+        m_TargetPosLastFrame = targetPos;
+	}
+	
+    /// <summary>
+    /// Does one brush action per call (per key press).
+    /// So It will do a brush up action then next time brush down then next up again etc.
+    /// </summary>
+    /// <param name="brushingMoveCurve"></param>
+    /// <param name="upDir"></param>
+    void BrushTooth1x_Toggled(Transform brushTr, Vector3 upDir){
+        StartCoroutine(LerpToTarget(
+            brushTr,
+            brushTr.position,
+            brushTr.position + m_brushingMotionWidth * upDir,
+            brushTr.rotation,
+            brushTr.rotation,
+            m_BrushTravelPosAnimCurve,
+            m_BrushTravelRotAnimCurve,
+            0, 0, 0,
+            animationDuration, m_SpeedModifier,
+            ()=>{
+                StartCoroutine(LerpToTarget(
+                    brushTr,
+                    brushTr.position,
+                    brushTr.position - m_brushingMotionWidth * upDir,
+                    brushTr.rotation,
+                    brushTr.rotation,
+                    m_BrushTravelPosAnimCurve,
+                    m_BrushTravelRotAnimCurve,
+                    0, 0, 0,
+                    animationDuration, m_SpeedModifier,
+                    ()=>{}
+                ));
+            }
+        ));
+    }
+
+	IEnumerator LerpToTarget(
+		Transform obj, 
+		Vector3 startPos, Vector3 targetPos, 
+		Quaternion startRot, Quaternion targetRot,
+		AnimationCurve moveCurve, AnimationCurve rotCurve,
+		float lerpT, float lerpVPos, float lerpVRot,
+		float duration, float speedMod,//, float travelDistance
+        Action doAfterReachedTarget
+		){
+		
+        lerpT = lerpT + (Time.deltaTime/(duration))*speedMod;
+        if(lerpT<1.0f){
+            lerpVPos = moveCurve.Evaluate(lerpT);
+            lerpVRot = rotCurve.Evaluate(lerpT);
+            obj.position = Vector3.Lerp(startPos, targetPos, lerpVPos);
+            obj.rotation = Quaternion.Lerp(startRot, targetRot, lerpVPos);
+            yield return new WaitForEndOfFrame();
+        }
+
+        obj.position = targetPos;
+        obj.rotation = targetRot;
+
+        //now we've reached the destination, now we can brush 1x
+        doAfterReachedTarget();
+	}
+}
---
+++ Assets/Scripts/ToothInfo.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToothInfo : MonoBehaviour
+{
+    public string lettersNeeded="";
+    [Range(0,1)]
+    public float dirtiness=0;
+    [SerializeField]
+    HandAndBrushMover m_mover;
+    SortedDictionary<float, int> m_SortedDistancesAndIndexes;
+    List<Transform> m_keyframeSource;
+    float m_maxDistance;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+
+        #region Distance Calculation
+        if(Vector3.Dot(transform.up, Vector3.up) > 0){
+            m_keyframeSource = m_mover.RotationKeyframes_BottomMouth;
+        }
+        else{
+            m_keyframeSource = m_mover.RotationKeyframes_TopMouth;
+        }
+        m_SortedDistancesAndIndexes = new SortedDictionary<float, int>();
+        #endregion
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Quaternion GetClosestBlendedKeyframeRotation(int numberOfClosestKeyframes = 2, bool forceRecalculate = false){
+        Quaternion targetRot = Quaternion.identity;
+        
+        #region Distance Calculation
+        if(forceRecalculate || m_SortedDistancesAndIndexes.Count == 0){
+            m_maxDistance = 0;
+            m_SortedDistancesAndIndexes = new SortedDictionary<float, int>();
+            for(int i=0; i<m_keyframeSource.Count; i++){
+                float dist = Vector3.Distance(transform.position, m_keyframeSource[1].position);
+                m_SortedDistancesAndIndexes.Add(dist, i);
+                if(dist>m_maxDistance)
+                    m_maxDistance = dist;
+            }
+        }
+        #endregion
+
+        //List<Quaternion> rotations = new List<Quaternion>();
+        Quaternion rot = Quaternion.identity;
+        for(int i = 0; i< numberOfClosestKeyframes; i++){
+            //rotations.Add(m_keyframeSource[m_SortedDistancesAndIndexes[0]].rotation);
+            rot = Quaternion.Lerp(
+                m_keyframeSource[m_SortedDistancesAndIndexes[0]].rotation,
+                rot,
+                m_SortedDistancesAndIndexes[0]/m_maxDistance
+                );
+        }
+        return rot;
+    }
+}
