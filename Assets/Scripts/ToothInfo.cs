99f184bf8653b183ae6e9cfc9db086e281550a7f
Tudor <this.tudor@gmail.com>
2019-04-27 17:39:09 +0200

    integrating movement, in levelTest2_tudor

--- Assets/Scripts/ToothInfo.cs
+++ Assets/Scripts/ToothInfo.cs
@@ -44,8 +44,17 @@ public class ToothInfo : MonoBehaviour
             m_maxDistance = 0;
             m_SortedDistancesAndIndexes = new SortedDictionary<float, int>();
             for(int i=0; i<m_keyframeSource.Count; i++){
-                float dist = Vector3.Distance(transform.position, m_keyframeSource[1].position);
-                m_SortedDistancesAndIndexes.Add(dist, i);
+                float dist = Vector3.Distance(transform.position, m_keyframeSource[i].position);
+                
+                if(m_SortedDistancesAndIndexes.ContainsKey(dist)){
+                    Debug.Log(dist+"; i: "+i);
+                    m_SortedDistancesAndIndexes.Add(dist+001f*i, i);
+                }
+                else
+                {
+                    m_SortedDistancesAndIndexes.Add(dist, i);
+                }
+
                 if(dist>m_maxDistance)
                     m_maxDistance = dist;
             }
@@ -54,14 +63,28 @@ public class ToothInfo : MonoBehaviour
 
         //List<Quaternion> rotations = new List<Quaternion>();
         Quaternion rot = Quaternion.identity;
-        for(int i = 0; i< numberOfClosestKeyframes; i++){
+        int c = 0;
+        //for(int i = 0; i< numberOfClosestKeyframes; i++){
+        foreach(KeyValuePair<float, int> entry in m_SortedDistancesAndIndexes){
+            if(c>=numberOfClosestKeyframes)
+                break;
+            c++;
             //rotations.Add(m_keyframeSource[m_SortedDistancesAndIndexes[0]].rotation);
+            //Debug.Log(entry.Key+", "+entry.Value);
+            m_keyframeSource[entry.Value].gameObject.SetActive(true);
+            StartCoroutine(DisableAfterTime(3,m_keyframeSource[entry.Value].gameObject));
+
             rot = Quaternion.Lerp(
-                m_keyframeSource[m_SortedDistancesAndIndexes[0]].rotation,
+                m_keyframeSource[entry.Value].rotation,
                 rot,
-                m_SortedDistancesAndIndexes[0]/m_maxDistance
+                entry.Key/m_maxDistance
                 );
         }
         return rot;
+    }
+
+    IEnumerator DisableAfterTime(float time, GameObject objToDisable){
+        yield return new WaitForSeconds(time);
+        objToDisable.SetActive(false);
     }
 }
