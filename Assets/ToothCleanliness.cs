e26ef1be0a674704202fd436f37448f4c75d27c5
ASUS-ALFA\nichi <mikkel@igda.dk>
2019-04-28 00:13:58 +0200

    d

--- Assets/ToothCleanliness.cs
+++ Assets/ToothCleanliness.cs
@@ -1,48 +1,41 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class ToothCleanliness : MonoBehaviour
-{
+public class ToothCleanliness : MonoBehaviour {
+    
+
     [Range(0, 1)]
-    [SerializeField] private float cleanliness;
-    [SerializeField] private float fadeOutTime = 1;
+    public float cleanliness;
+    [SerializeField] float fadeOutTime = 1;
     public Material mat;
 
-    public float Cleanliness
-    {
-        get => cleanliness;
-        set => StartFadeTo(value);
-    }
-
-    private void Start()
-    {
+    private void Start() {
         mat = gameObject.GetComponent<Renderer>().material;
+
         mat.SetFloat("_NoiceSeed", Random.Range(3f, 50f));
+
     }
 
-    private void StartFadeTo(float value)
-    {
-        float difference = Mathf.Abs(cleanliness - value);
-        difference = Mathf.Clamp01(difference);
-
-        float duration = difference * fadeOutTime;
-        IEnumerator fadeRoutine = FadeOutDirt(value, duration);
-        StartCoroutine(fadeRoutine);
+    public void ToothCleaned() {
+        StartCoroutine(FadeOutDirt());
     }
 
-    private IEnumerator FadeOutDirt(float targetValue, float duration)
-    {
-        float startValue = cleanliness;
 
-        float elapsedTime = 0;
-        while (elapsedTime < duration)
-        {
-            float point = elapsedTime / duration;
-            cleanliness = Mathf.Lerp(startValue, targetValue, point);
+    private void Update() {
+        mat.SetFloat("_fadeValue", cleanliness);
+    }
+
+
+    IEnumerator FadeOutDirt() {
+        // fade from opaque to transparent
+
+        // loop over 1 second backwards
+        for ( ; fadeOutTime <= 1; fadeOutTime += Time.deltaTime) {
+            cleanliness = fadeOutTime;
+            // set color with i as alpha
             mat.SetFloat("_fadeValue", cleanliness);
-
             yield return null;
-            elapsedTime += Time.deltaTime;
         }
     }
 }
